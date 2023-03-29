1c9dfe7624bbe9550b7de7cf00436bc388517f58
Jayanth Kumar K H <122151719+jayanth40@users.noreply.github.com>
2023-03-29 10:11:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(cookieParser())
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    
+      allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
   
